1cce2372407c71da2a430335f9a14cf6b9eb0eee
arielm81 <arielm81@gmail.com>
2025-05-11 14:50:31 -0300

    Cambios en ChatApp

--- chatapp-frontend/vite.config.js
+++ chatapp-frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Asegura que los cambios en los archivos sean detectados
+    },
+  },
 })
